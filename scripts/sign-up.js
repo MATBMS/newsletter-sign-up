8a754091453981b58f5b599c8713ea04bc82517f
Mathieu Bosmans <43988246+MATBMS@users.noreply.github.com>
2025-12-10 08:45:34 +0100

    Improve Accessibility closes #6

--- scripts/sign-up.js
+++ scripts/sign-up.js
@@ -1,7 +1,7 @@
 // Define variables
 const form = document.getElementById('sign-up-form');
 const emailInput = document.getElementById('email');
-const errorMessage = document.querySelector('.error-message');
+const errorMessage = document.getElementById('error-message');
 
 // Add event listener to the form
 form.addEventListener('submit', function (event) {
@@ -17,11 +17,14 @@ form.addEventListener('submit', function (event) {
     // Show error message
     errorMessage.classList.remove('hide');
     emailInput.classList.add('input-error');
+    emailInput.setAttribute('aria-invalid', 'true');
+    // Focus the email input for accessibility
+    emailInput.focus();
   } else {
     // Hide error message and proceed with form submission
     errorMessage.classList.add('hide');
     emailInput.classList.remove('input-error');
-
+    emailInput.setAttribute('aria-invalid', 'false');
     // Store the email in localStorage to use on success page
     localStorage.setItem('userEmail', emailValue);
 
